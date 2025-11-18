a1b47afe16f1811c636ac219f588338d9c7d4272
Amaan <amaanshaikh.gg@gmail.com>
2025-11-19 00:10:57 +0530

    add types

---
+++ src/types/notices.ts
@@ -0,0 +1,11 @@
+export type NoticeItem = {
+  title: string;
+  link: string;
+  slug: string;
+  date: string;
+  shortDescription: string;
+  overview: string;
+    keyDetails: string[];
+    guidelines: string[];
+   
+};
